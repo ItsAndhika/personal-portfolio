5834f05d851ba722afa12610520c8d4374ee2869
ItsAndhika <bambangindaryanto12@gmail.com>
2023-04-03 19:58:41 +0800

    setup the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-blue-600 font-bold text-6xl text-center">React + Tailwindcss</h1>
+    </div>
+  )
+}
+
+export default App
