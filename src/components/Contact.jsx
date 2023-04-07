5d37a914b7f6670e5ab58f87f43988ce26e2db0c
ItsAndhika <bambangindaryanto12@gmail.com>
2023-04-07 12:51:08 +0800

    fix bug while send the contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,14 +13,21 @@ const Contact = () => {
         <label htmlFor="message" className="font-semibold mx-auto sm:text-xl lg:text-2xl lg:mt-5 lg:block 2xl:text-3xl">Message</label>
         <textarea type="message" name="message" id="message" placeholder="Your message..." required className="form-input lg:h-36"/>
         <button type="submit" id='send' className='w-5/12 mx-auto text-center text-lg font-medium bg-blue-600 rounded-full px-5 py-3 mt-5 lg:w-40 hover:bg-blue-500 active:bg-blue-700' onClick={() => {
+          const scriptURL = 'https://script.google.com/macros/s/AKfycbxk5wFU7JdMUY-BoHAQeMZQ9gp3dl0U2-gMl0cdm1AYlC-Pf642oRXti-hwKmThRDs/exec'
+          const form = document.forms['contact-form']
+          const sendButton = document.getElementById('send')
           const alert = document.getElementById('alert')
-          fetch('https://script.google.com/macros/s/AKfycbxk5wFU7JdMUY-BoHAQeMZQ9gp3dl0U2-gMl0cdm1AYlC-Pf642oRXti-hwKmThRDs/exec', { method: 'POST', body: new FormData(form)})
-            .then(response => {
-                alert.classList.toggle('hidden')
-                form.reset()
-                console.log('Success!', response)
-            })
-            .catch(error => console.error('Error!', error.message))
+
+          form.addEventListener('submit', e => {
+            e.preventDefault()
+            fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+              .then(response => {
+                  alert.classList.toggle('hidden')
+                  form.reset()
+                  console.log('Success!', response)
+              })
+              .catch(error => console.error('Error!', error.message))
+          })
         }}>Send</button>
       </form>
     </div>  
