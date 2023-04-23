69accf29e6dc0d2bd2a6a5c929b5b356b42a5ea5
ItsAndhika <bambangindaryanto12@gmail.com>
2023-04-23 16:32:08 +0700

    fix bug site blank when opened

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import AOS from 'aos'
+import 'aos/dist/aos.css';
 import App from './App'
 import './index.css'
+
+AOS.init();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
